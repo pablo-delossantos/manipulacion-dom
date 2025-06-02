55136e6f437525b71f0aaf0aad43b0c1ec203490
Pablo de los Santos <estudiodedalo@gmail.com>
2025-06-02 17:35:44 -0300

    Eventos mouse y click

---
+++ 05-Events/app.js
@@ -0,0 +1,25 @@
+const container = document.querySelector(".container")
+const button = document.querySelector("button")
+
+// Evento, código que se ejecuta
+container.addEventListener("mouseover", () => {
+  container.style.backgroundColor = "#3f51b5"
+})
+
+container.addEventListener("mouseout", () => {
+  container.style.backgroundColor = "#29d492"
+})
+
+// button.addEventListener("click", () => {
+//   alert("Button clicked")
+// })
+
+const buttonClickCallback = () => {
+  alert("Button clicked")
+}
+
+button.addEventListener("click", buttonClickCallback)
+
+setTimeout(() => {
+  button.removeEventListener("click", buttonClickCallback)
+}, 2000)
