2647421d7b01531ed88ad8f3c56c78792f239fd6
Pablo de los Santos <estudiodedalo@gmail.com>
2025-06-01 21:07:51 -0300

    Clonacion y reemplazo de elementos

--- 04-InnerHtml/app.js
+++ 04-InnerHtml/app.js
@@ -14,3 +14,22 @@ const newPElement = document.createElement("p")
 newPElement.textContent = "Fui creado con CreateElement"
 
 contentArea.append(newPElement) // Inyecta en el HTML
+
+// Remover elementos
+
+const firstItem = document.querySelector("li")
+firstItem.remove() // remueve el primer item
+
+const list = document.querySelector("ul")
+list.removeChild(list.firstElementChild) // Remueve el primero del contenedor, en este caso del <ul>
+
+// Clonar elementos
+const originalP = contentArea.querySelector("p")
+const clonedP = originalP.cloneNode(true) // Lo clonamos
+
+contentArea.append(clonedP)
+clonedP.textContent = "Este es un nuevo texto." // Lo editamos
+
+// Reemplazar elementos
+const itemToReplace = listArea.children[2]
+itemToReplace.replaceWith(clonedP) // Reemplazamos con el texto clonado
